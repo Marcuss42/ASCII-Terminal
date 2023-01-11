a4a7e825472fcff1e861d0aee10b644b848be128
Marcuss42 <105746798+Marcuss42@users.noreply.github.com>
2023-01-10 21:34:07 -0300

    s
    s

---
+++ dvd.py
@@ -0,0 +1,48 @@
+from os import system
+from time import sleep
+from colorama import Fore, Style
+from random import choice
+
+COLORS = dir(Fore)[1:17]
+COR_TELA = Fore.BLUE
+DVD = 'DVD'
+SIZE = len(DVD)
+
+I, J = 11, 11
+tela = [ [' ' * SIZE for j in range(J)] for i in range(I) ]
+
+def main():
+    vi, vj = 1, 2
+    i, j = 1, 4
+
+    cor_dvd = Fore.BLUE
+
+    while True:
+        tela[i][j] = cor_dvd + DVD + COR_TELA
+    
+        print(COR_TELA + '█' * (2+J*SIZE))
+        for linha in tela:
+            print('█' + ''.join(linha) + '█')
+        print('█' * (2+J*SIZE))
+        
+        tela[i][j] = ' ' * SIZE
+        i += vi
+        j += vj
+
+        if i+vi >= I or i <= 0:
+            vi = -vi
+            cor_dvd = getattr(Fore,choice(COLORS))
+        if j+vj >= J or j <= 0:
+            vj = -vj
+            cor_dvd = getattr(Fore,choice(COLORS))
+
+        sleep(.5)
+        system('cls || clear')
+
+if __name__ == "__main__":
+    system('cls || clear')
+    try:
+        main()
+    except:
+        print(Style.RESET_ALL)
+    
